4c7acedcdc3802ea358ebd3c90d16a71bec5eb6b
shahin hossain <shahinhossain3@hotmail.com>
2023-12-23 11:36:39 +0600

    Create Read and delete And Show Remaining Users

--- __steps__.js
+++ __steps__.js
@@ -1,4 +1,5 @@
 /**
+ * modgodb connection
  * mongo atlas continue with google
  * express server create -> express, cors, mongodb
  * scripts add -> "start": "node index.js"
@@ -21,3 +22,58 @@
  * 
  *
  */
+
+/**
+ * ----------------
+ * MongoDB Connection
+ * ----------------
+ * 1. create account
+ * 2. crate an user with password 
+ * 3. whitelist IP Address
+ * 4. database > connect > driver > Node >view full code > 
+ * 5. changed the password in the uri
+ * -----------------------------
+ * 1. CREATE ----POST
+ * 2. app.post('/users', async (req, res) => {})
+ * 3. Make a function async to use await inside it.
+ * 4. Make sure you use the express.json() middleware
+ * 5. access data from body const user = req.body
+ * 6. const result = await userCollection.insertOne(user);
+ * 7. res.send(result)
+ * 
+ * CLIENT
+ * -------------------
+ * CREATE
+ * ------------------
+ * 1. crate fetch
+ * 2. add second parameter /options
+ * 3. Provide Method "POST"
+ * 4. add header : {"content-type": "application/json"}
+ * 5. add body: json.stringify(user)
+ *
+ * 
+ * -------------------
+ * READ MANY
+ * -------------------
+ * crate a cursor = userCollection.find()
+ * const result = await cursor.toArray();
+ * 
+ * -----------------------
+ * DELETE
+ * --------------------
+ * crate app.delete('/users/id', async(req, res) => {})
+ * specify unique ObjectId to delete the right user
+ * const query = {_id: ObjectId(id)}
+ * const result = await movies.deleteOne(query);
+ * 
+ * ClIENT
+ * 
+ * create dynamic url with id
+ * mention the DELETE method
+ * 
+ * user delete from UI
+ * crate a state & initial state is loaded users
+ * user filter with id & remaining users set to users State
+ *  
+ *  */
+
